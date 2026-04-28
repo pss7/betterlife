ddbbfeda253184a561292013802e24c2e1413a69
parksangseok <park0157@naver.com>
2026-04-28 11:49:34 +0900

    commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+$(function () {
+
+    $('.bannerBox .slick').slick({
+    autoplay: false,
+    arrows: false,
+    dots: true,
+    accessibility: false,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    zIndex: 1000,
+    pauseOnHover: false,
+    autoplaySpeed: 3500,
+    speed: 1100,
+  });
+
+
+
+
+
+
+});
---
+++ js/sub.js
@@ -0,0 +1,13 @@
+
+$(function () {
+
+  //게시글 수정, 삭제 버튼
+  $('.boardViewBox .boardViewPostOptionBtn').click(function () {
+    $('.boardViewPostOptionMenu').toggleClass('active');
+  });
+
+
+
+
+
+});
